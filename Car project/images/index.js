7aa8795c84691ebd1b4d8c7c1da8397e9b63fd7d
ANDERSON DANIEL TSENDAKI <andersontdaniel99@gmail.com>
2026-02-07 01:07:22 +0100

    Add files via upload

---
+++ Car project/images/index.js
@@ -0,0 +1,128 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+document.querySelector("#login-btn").onclick = () => {
+  document.querySelector(".login-form-container").classList.toggle("active");
+};
+
+document.querySelector("#close-login-form").onclick = () => {
+  document.querySelector(".login-form-container").classList.remove("active");
+};
+
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+// Parallax Effect
+document.querySelector(".home").onmousemove = (e) => {
+  document.querySelectorAll(".home-parallax").forEach((el) => {
+    let speed = el.getAttribute("data-speed");
+    let x = (window.innerWidth - e.pageX * speed) / 90;
+    let y = (window.innerHeight - e.pageY * speed) / 90;
+
+    // Fixed: variable was 'elm' in your code, changed to 'el'
+    el.style.transform = `translate(${x}px, ${y}px)`;
+  });
+};
+
+document.querySelector(".home").onmouseleave = () => {
+  document.querySelectorAll(".home-parallax").forEach((el) => {
+    el.style.transform = "translate(0,0)";
+  });
+};
+
+// Swiper for Vehicles
+var swiperVehicles = new Swiper(".vehicles-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true, // "gapCursor" is not valid, changed to grabCursor
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Swiper for Featured Cars
+var swiperFeatured = new Swiper(".featured-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Swiper for Reviews
+var swiperReview = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
